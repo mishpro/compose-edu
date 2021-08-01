13077f096a6461e53296c2079933edaf3c810a4c
mish@temp <mike.prokopski@gmail.com>
2021-08-01 22:22:13 +0300

    add docker-compose.yml; -check folder bind starting from other program

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,4 @@ FROM continuumio/anaconda3
 
 WORKDIR /usr/local/bin/
 COPY recvall.py s.py ./
-ENTRYPOINT "python" "s.py"
+# ENTRYPOINT "python" "s.py"
--- cl.py
+++ cl.py
@@ -10,7 +10,8 @@ address = ('localhost', 8787)
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
 shared = checknmake(script_dir)
-complete = sp.run('docker run --name=exp-logs -dp 8787:8787 -v '+shared+':/_shared exp/server-2', shell=True)
+# complete = sp.run('docker run --name=exp-logs -dp 8787:8787 -v '+shared+':/_shared exp/server-2', shell=True)
+sp.run('docker-compose up -d', shell=True)
 mkfiles(shared, 'file_')
 
 
@@ -21,7 +22,8 @@ with socket.create_connection(address) as sock:
     print(data1, data2)
 
 print('_____________')
-sp.run('docker logs exp-logs', shell=True)
+print('[compose] Logs from container')
+sp.run('docker-compose logs', shell=True)
 print('_____________')
-sp.run('docker stop exp-logs', shell=True)
-sp.run('docker rm exp-logs',shell=True)
+# sp.run('docker-compose down', shell=True)
+# sp.run('docker rm exp-logs',shell=True)
